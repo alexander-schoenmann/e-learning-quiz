2199cc237aeb8a049105f2b9b90210a63c5cc0d3
ver_sch <verena.schickmair@hotmail.com>
2022-04-14 08:52:25 +0200

    Radiobuttons and Checkboxes

--- ELearningQuiz/src/app/question-radio/question-radio.component.ts
+++ ELearningQuiz/src/app/question-radio/question-radio.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class QuestionRadioComponent implements OnInit {
   @Input() question: any;
+
   constructor() {}
 
   ngOnInit(): void {}

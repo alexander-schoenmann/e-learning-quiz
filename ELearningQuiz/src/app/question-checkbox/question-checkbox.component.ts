7bd4d3ce188b10c1a8dc342a048335ce9b1fd265
jorle31 <s1910456022@students.fh-hagenberg.at>
2022-04-16 17:49:18 +0200

    checkbox-qeustion

--- ELearningQuiz/src/app/question-checkbox/question-checkbox.component.ts
+++ ELearningQuiz/src/app/question-checkbox/question-checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-question-checkbox',
@@ -7,9 +7,94 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class QuestionCheckboxComponent implements OnInit {
   @Input() question: any;
-  constructor() { }
 
-  ngOnInit(): void {
+
+  public answerValues : string[] = [];
+
+  public currentQuestion = 0;
+  public selected : number[] = [0, 0, 0];
+
+  public selectedAnswer: string = "";
+  public selectedAnswerTexts: string[] = ['', '', ''];
+  public selectedAnswerDescriptions: string[] = ['', '', ''];
+
+  public showHintToAnswerQuestion = false;
+  public showCorrectAnswerField = false;
+  public showFalseAnswerField = false;
+  public showPartiallyRightAnswerField = false;
+  public hideInputs = false;
+
+  public showSubmitButton = true;
+  public showNextButton = false;
+
+  @Output() updateResultCounter = new EventEmitter<any>();
+  @Output() updateCorrectAnswerAmount = new EventEmitter<number>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public calculateResult () {
+    if (this.answerValues.length >= 1) {
+      if (this.answerValues.filter(item => item == "false").length == 0
+        && this.answerValues.filter(item => item == "true").length == 2) {
+        //richtig
+        this.showHintToAnswerQuestion = false;
+        this.showFalseAnswerField = false;
+        this.showPartiallyRightAnswerField = false;
+        this.showCorrectAnswerField = true;
+
+      } else if(this.answerValues.filter(item => item == "false").length == 0
+        && this.answerValues.filter(item => item == "true").length < 2){
+        this.showPartiallyRightAnswerField = true;
+        this.showFalseAnswerField = false;
+        this.showHintToAnswerQuestion = false;
+        this.showCorrectAnswerField = false;
+      }
+      else {
+        //false
+        this.showHintToAnswerQuestion = false;
+        this.showPartiallyRightAnswerField = false;
+        this.showCorrectAnswerField = false;
+        this.showFalseAnswerField = true;
+      }
+      this.hideInputs = true;
+
+      this.showSubmitButton = false;
+      this.showNextButton = true;
+
+    } else {
+      this.showHintToAnswerQuestion = true;
+    }
   }
 
+  public onItemSelection (value: any) {
+    this.selectedAnswer = value.value;
+    this.currentQuestion = value.getAttribute("data-answerId");
+    if (this.selected[this.currentQuestion] == 0){
+      this.answerValues[this.currentQuestion] = this.selectedAnswer;
+      this.selectedAnswerTexts[this.currentQuestion] = value.getAttribute("data-answerText");
+      this.selectedAnswerDescriptions[this.currentQuestion] = value.getAttribute("data-answerDescription");
+      this.selected[this.currentQuestion] = 1;
+    }
+    else {
+      this.answerValues[this.currentQuestion] = '';
+      this.selectedAnswerTexts[this.currentQuestion] = '';
+      this.selectedAnswerDescriptions[this.currentQuestion] = '';
+      this.selected[this.currentQuestion] = 0;
+    }
+  }
+
+  public navigateToNextQuestion () {
+    this.updateResultCounter.emit();
+    let counter = 0;
+
+    if (this.selectedAnswer == "true") {
+      counter +=1;
+      this.updateCorrectAnswerAmount.emit(counter);
+    } else {
+      this.updateCorrectAnswerAmount.emit(counter);
+    }
+  }
 }
+

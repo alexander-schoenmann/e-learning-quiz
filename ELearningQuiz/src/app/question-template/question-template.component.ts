e947cca8cbb42df530f3b61fc30486ad612d3e15
Verena <verena.schickmair@hotmail.com>
2022-04-19 17:31:59 +0200

    Checkbox subtitle

--- ELearningQuiz/src/app/question-template/question-template.component.ts
+++ ELearningQuiz/src/app/question-template/question-template.component.ts
@@ -17,8 +17,18 @@ export class QuestionTemplateComponent implements OnInit {
       type: 'radio',
       title: 'Findest du das Passwort "Michael1998" sicher?',
       answers: [
-        { text: 'Ja', correct: false, description: "Das Passwort sollte keine personenbezogenen Inhalte wie Name oder Passwort sein." },
-        { text: 'Nein', correct: true, description: "Das Passwort sollte keine personenbezogenen Inhalte wie Name oder Passwort sein." },
+        {
+          text: 'Ja',
+          correct: false,
+          description:
+            'Das Passwort sollte keine personenbezogenen Inhalte wie Name oder Passwort sein.',
+        },
+        {
+          text: 'Nein',
+          correct: true,
+          description:
+            'Das Passwort sollte keine personenbezogenen Inhalte wie Name oder Passwort sein.',
+        },
       ],
     },
     {
@@ -27,30 +37,57 @@ export class QuestionTemplateComponent implements OnInit {
       title: 'Handelt es sich hierbei um eine Phishing E-Mail?',
       image: '../../assets/images/example.jpg',
       answers: [
-        { text: 'Ja', correct: true, description: "Wir verwenden Global Pay nicht in unserem Unternehmen. Der Anhang enthält somit wahrscheinlich Schadsoftware."},
-        { text: 'Nein', correct: false, description: "Wir verwenden Global Pay nicht in unserem Unternehmen. Der Anhang enthält somit wahrscheinlich Schadsoftware." },
+        {
+          text: 'Ja',
+          correct: true,
+          description:
+            'Wir verwenden Global Pay nicht in unserem Unternehmen. Der Anhang enthält somit wahrscheinlich Schadsoftware.',
+        },
+        {
+          text: 'Nein',
+          correct: false,
+          description:
+            'Wir verwenden Global Pay nicht in unserem Unternehmen. Der Anhang enthält somit wahrscheinlich Schadsoftware.',
+        },
       ],
     },
     {
       id: 3,
       type: 'checkbox',
       title: 'Welche Vorteile bringt ein VPN mit sich?',
-      subtitle: 'Mehrere Antworten möglich!',
+      subtitle: 'Bitte wähle mehrere Antworten aus.',
       answers: [
-        { text: 'Sichere Verbindung für Fernarbeit', correct: true, description: "Unternehmen müssen heute mehr denn je auf die Sicherheit im Internet achten. Da mehr Menschen als je zuvor von zu Hause aus arbeiten, sind sensible Unternehmens- und Kundendaten einem höheren Risiko des Diebstahls ausgesetzt.", answerId : 0},
-        { text: 'Sicherheit durch Anonymität', correct: true, description: "Mit einer VPN wird dein Standort anonym gehalten.", answerId : 1},
-        { text: 'Schutz vor Malware', correct: false, description: "Ein VPN schützt ein Gerät nicht vor den meisten Formen von Malware (wie Viren, Spyware und Ransomware).", answerId : 2},
+        {
+          text: 'Sichere Verbindung für Fernarbeit',
+          correct: true,
+          description:
+            'Unternehmen müssen heute mehr denn je auf die Sicherheit im Internet achten. Da mehr Menschen als je zuvor von zu Hause aus arbeiten, sind sensible Unternehmens- und Kundendaten einem höheren Risiko des Diebstahls ausgesetzt.',
+          answerId: 0,
+        },
+        {
+          text: 'Sicherheit durch Anonymität',
+          correct: true,
+          description: 'Mit einer VPN wird dein Standort anonym gehalten.',
+          answerId: 1,
+        },
+        {
+          text: 'Schutz vor Malware',
+          correct: false,
+          description:
+            'Ein VPN schützt ein Gerät nicht vor den meisten Formen von Malware (wie Viren, Spyware und Ransomware).',
+          answerId: 2,
+        },
       ],
     },
   ];
 
   ngOnInit(): void {}
 
-  updateProgress () {
+  updateProgress() {
     this.progress += 1;
   }
 
-  updateCorrectAnswerCounter (number: number) {
+  updateCorrectAnswerCounter(number: number) {
     this.correctAnswers += number;
   }
 }
